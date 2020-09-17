401c1b9612ab3d116fdba308fe23c402c60fe3b2
theskymaks <theskymaks@gmail.com>
2020-09-18 00:01:28 +0300

    Add project files.

---
+++ IP_calculator.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace IP_calculator.Droid
+{
+    [Activity(Label = "IP_calculator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        /// <summary>
+        /// Start of project
+        /// </summary>
+        /// <param name="savedInstanceState"></param>
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            //Здесь производится инициализация платформ Xamarin.Essentials и Xamarin.Forms:
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            //экземпляр класса App передается в метод LoadApplication(), который определен у класса Xamarin.Forms.Platform.Android.FormsAppCompatActivity, 
+            // и собственно происходит запуск приложения Xamarin Forms:
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ IP_calculator/StartPage.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace IP_calculator
+{
+    class StartPage : ContentPage
+    {        
+        public StartPage()
+        {
+            Label header = new Label() { Text = "Привет из Xamarin Forms" };
+            Content = header;
+            //Данный класс представляет страницу, поэтому наследуется от класса ContentPage. 
+            //В конструкторе создается метка с текстом, которая задается в качестве содержимого страницы (Content = header).
+        }
+    }
+}
